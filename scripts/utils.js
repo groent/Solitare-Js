96f272e2bb4a71142a27b9097c68d5f3feb057c2
groenp <groenp@outlook.com>
2024-02-16 21:58:10 -0500

    commenting

--- scripts/utils.js
+++ scripts/utils.js
@@ -5,14 +5,14 @@ export var controller = new AbortController();
 
 // *****************************************************************
 export function sleep(ms) {  // Pause execution for x msec
-// Has to be called with "await sleep(xxx)"
-// Creates a pause before further execution. 
-// Can be used for animation purposes by stopping a loop. 
-// *****************************************************************
-return new Promise(resolve => setTimeout(resolve, ms));
-} // end of: sleep()
-
-
+    // Has to be called with "await sleep(xxx)"
+    // Creates a pause before further execution. 
+    // Can be used for animation purposes by stopping a loop. 
+    // *****************************************************************
+    return new Promise(resolve => setTimeout(resolve, ms));
+    } // end of: sleep()
+    
+    
 // *****************************************************************
 export function generateId(len = 4) {  // Generate code with alphanumeric characters
 // Code has 'len' length, default 4. Caps only, O & 0 and I & 1 omitted for clarity.
@@ -62,12 +62,77 @@ export function showThrow(el, eyes) {  // turn the die (dom el) so the number of
 /***                      COOKIE  FUNCTIONS                          ***/
 /***********************************************************************/
 
+
+// *****************************************************************
+function storage() { // Check if local (and session) storage is supported, returns true or false
+// *****************************************************************
+
+    // because of iOS an actual attempt has to be made, this function may run before iOSprivMode()
+    try {
+        localStorage.setItem("test", "1");
+        localStorage.removeItem("test");
+    } catch (e) {
+        return false;
+    }
+    // return false; // DEBUG // force alternative path...
+    return 'localStorage' in window && window['localStorage'] !== null;
+} // /storage()
+
+
+// *****************************************************************
+export function readCkie(name) { // Returns a cookie by name ('name=') as a string, 'name' has to be string
+// *****************************************************************
+    // if (false) { // DEBUG //
+    if (storage() == true) {
+        return localStorage.getItem(name);                                                              // return cookie
+    } else {
+        var a_ckie = document.cookie.split("; ");                                                       // split domain cookies and their sections
+        for (var i = 0; i < a_ckie.length; i++) {                                                       // for each cookie section
+            while (a_ckie[i].charAt(0) == " ") a_ckie[i] = a_ckie[i].substring(1, a_ckie[i].length);    // strip any spaces before cookie name
+            if (a_ckie[i].indexOf(name + "=") == 0) {                                                   // if right cookie found
+                return decodeURIComponent(a_ckie[i].substring(name.length + 1, a_ckie[i].length));                // return the section after '='
+            }
+        }
+    }
+    return "";  // returns empty because the result might be parsed into a string
+}  // end of: readCkie()
+
+
+// *****************************************************************
+export function writeCkie(name, data, days) { // Write cookie with name (as a string) and data, use of days is optional (only works with old cookie system)
+// *****************************************************************
+    // if (false) { // DEBUG //
+    if (storage() == true) {
+        localStorage.setItem(name, data);                                                               // persistence (local) storage is always unlimited
+    } else {
+        var date_today = new Date();                                                                    // today's date, for expiry calculation
+        var date_expire = (days) ? new Date(date_today.valueOf() + (days * 86400000)) : null;           // expiry date (= today + exp days in msec)
+        var s_ckie = name + "=" + encodeURIComponent(data);                                                         // cookie will be named 'name' DON'T ESCAPE THE '='sign!
+
+        if (days) s_ckie += "; expires=" + date_expire.toGMTString();                                   // and add expiry date in proper format (only when defined)
+        s_ckie += "; path=/"                                                                            // set path to use cookie from pages in different dir's
+        document.cookie = s_ckie;                                                                       // set the cookie
+    }
+}  // end of: writeCkie()
+
+
+// *****************************************************************
+export function clearCkie(name) { // Wipes cookie, could be used in conjunction with cookie existence check with writeCkie() 
+// *****************************************************************
+if (storage() == true) {
+        localStorage.removeItem(name);
+    } else {
+        writeCkie(name, "", -1);
+    }
+}  // end of: clearCkie()
+
+
 /******************************************************************************/
 // don't use sessionStorage, use localStorage instead for simplicity,         //
 // it is supported by all browsers and devices, and thus no need for wrappers //
 /******************************************************************************/
 
-function storage() { // Check if local (and session) storage is supported, returns true or false
+function storageOLD() { // Check if local (and session) storage is supported, returns true or false
 
     // because of iOS an actual attempt has to be made, this function may run before iOSprivMode()
     try {
@@ -118,45 +183,3 @@ function clrGlbl(name) {        // clearGlobal var, name is string; usage: name 
     if (storage()) sessionStorage.removeItem(name);
     return "";
 } // /clrGlbl()
-
-
-export function readCkie(name) { // Returns a cookie by name ('name=') as a string, 'name' has to be string
-    // if (false) {
-    if (storage() == true) {
-        return localStorage.getItem(name);                                                              // return cookie
-    } else {
-        var a_ckie = document.cookie.split("; ");                                                       // split domain cookies and their sections
-        for (var i = 0; i < a_ckie.length; i++) {                                                       // for each cookie section
-            while (a_ckie[i].charAt(0) == " ") a_ckie[i] = a_ckie[i].substring(1, a_ckie[i].length);    // strip any spaces before cookie name
-            if (a_ckie[i].indexOf(name + "=") == 0) {                                                   // if right cookie found
-                return decodeURIComponent(a_ckie[i].substring(name.length + 1, a_ckie[i].length));                // return the section after '='
-            }
-        }
-    }
-    return "";  // returns empty because the result might be parsed into a string
-} // /readCkie()
-
-
-export function writeCkie(name, data, days) { // Write cookie with name (as a string) and data, use of days is optional (only works with old cookie system)
-    // if (false) {
-    if (storage() == true) {
-        localStorage.setItem(name, data);                                                               // persistence (local) storage is always unlimited
-    } else {
-        var date_today = new Date();                                                                    // today's date, for expiry calculation
-        var date_expire = (days) ? new Date(date_today.valueOf() + (days * 86400000)) : null;           // expiry date (= today + exp days in msec)
-        var s_ckie = name + "=" + encodeURIComponent(data);                                                         // cookie will be named 'name' DON'T ESCAPE THE '='sign!
-
-        if (days) s_ckie += "; expires=" + date_expire.toGMTString();                                   // and add expiry date in proper format (only when defined)
-        s_ckie += "; path=/"                                                                            // set path to use cookie from pages in different dir's
-        document.cookie = s_ckie;                                                                       // set the cookie
-    }
-} // /writeCkie()
-
-
-export function clearCkie(name) { // Wipes cookie, could be used in conjunction with cookie existence check with writeCkie() 
-    if (storage() == true) {
-        localStorage.removeItem(name);
-    } else {
-        writeCkie(name, "", -1);
-    }
-} // /clearCkie()
